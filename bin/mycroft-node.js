1a62dcfd31574d74b7acde79aa7a4c357748fd86
weswigham <wwigham@gmail.com>
2014-03-18 14:55:38 -0400

    CLI for template generation

--- bin/mycroft-node.js
+++ bin/mycroft-node.js
@@ -1,18 +1,66 @@
 #! /usr/bin/env node
 
 var Metalsmith = require('metalsmith');
-var prompt = require('cli-prompt');
+var prompt = require('prompt');
 var render = require('consolidate').handlebars.render;
+var async = require('async');
+var argv = require('minimist')(process.argv.slice(2));
+
+var usage = "usage: mycroft-node new [foldername]";
+
+if ((argv._.length<1) || argv._[0]!=='new') {
+  console.log(usage);
+  return;
+}
+
+var dir = process.cwd();
+if (argv._.length>=2) {
+  dir += "\\\\"+argv._[1];
+}
 
 var m = new Metalsmith('/');
 m.dir = ''; //Erhmahgerd
 m.source(__dirname+'\\\\template');
-m.destination(process.cwd());
+m.destination(dir);
 m.use(ask);
 m.use(template);
 m.build(function(err){
   if (err) throw err;
 });
+
+var info_schema = {
+  properties: {
+    name: {
+      pattern: /^[a-zA-Z\-\_]+$/,
+      message: "Your application's code-safe name (letters, dashes, underscores)",
+      required: true
+    },
+    fancyname: {
+      message: "Your application's display name",
+      required: true
+    },
+    instance: {
+      message: "A default instanceId for your application",
+      default: "instance"
+    },
+    author: {
+      message: "Your name",
+      required: true
+    },
+    license: {
+      message: "Your application's license",
+      default: "BSD"
+    },
+    desc: {
+      message: "Your application's description",
+      default: ''
+    }, 
+    repo: {
+      message: "Your application's repository",
+      default: ''
+    }
+  }
+};
 
 /**
 * Prompt plugin.
@@ -23,17 +71,18 @@ m.build(function(err){
 */
 
 function ask(files, metalsmith, done){
-  var prompts = ['name', 'author', 'license', 'desc', 'repo'];
   var metadata = metalsmith.metadata();
 
-  async.eachSeries(prompts, run, done);
-
-  function run(key, done){
-    prompt(' ' + key + ': ', function(val){
-      metadata[key] = val;
-      done();
-    });
-  }
+  prompt.start();
+  prompt.get(info_schema, function(err, res) {
+    if (!err) {
+      for (var key in res) {
+        var value = res[key];
+        metadata[key] = value;
+      }
+    }
+    done();
+  });
 }
 
 /**
